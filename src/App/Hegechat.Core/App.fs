4b164085020cc80f768191f6ae9920185dfa3c97
mikhail.smirnov@bimgen.app <mikhail.smirnov@bimgen.app>
2022-05-03 15:56:32 +0700

    Input text.

--- src/App/Hegechat.Core/App.fs
+++ src/App/Hegechat.Core/App.fs
@@ -1,10 +1,74 @@
 module Hegechat.Core.App
 
 open Sutil
+open DOM
+open Attr
+open Bulma
+
+module EventHelpers =
+    open Browser.Types
+
+    let inputElement (target:EventTarget) = target |> asElement<HTMLInputElement>
+
+    let validity (e : Event) =
+        inputElement(e.target).validity
+
+type Model =
+    { Counter : int
+      Text: string }
+
+type Message =
+    | Increment
+    | Decrement
+    | SetText of string
+
+let init () =
+    { Counter = 0
+      Text = "Init" }, []
+
+let update msg model =
+    match msg with
+    | Increment -> { model with Counter = model.Counter + 1 }, Cmd.none
+    | Decrement -> { model with Counter = model.Counter - 1 }, Cmd.none
+    | SetText t -> { model with Text = t }, Cmd.none
 
 let app() =
+   
+    let model, dispatch = () |> Store.makeElmish init update ignore
+    
     Html.div [
+        disposeOnUnmount [ model ]
+        
+        style [
+            Css.fontFamily "Arial, Helvetica, sans-serif"
+            Css.margin 20
+        ]
+        
         Html.h1 "Hello Hegechat"
+        
+        Bind.fragment (Store.map (fun m -> m.Counter) model) (fun n -> Html.text $"Counter = {n}")
+        
+        
+        bulma.field.div [
+            bulma.control.div [
+                bulma.input.text [
+                    Bind.attr("value", model .> (fun m -> m.Text), SetText >> dispatch)
+                    Bind.attr("value", model .> (fun m -> m.Text), (fun x -> dispatch Increment ) )
+                    Attr.required true]  ] ]
+        
+        Html.div [
+            Html.button [
+                class' "button"
+                onClick (fun _ -> dispatch Decrement) []
+                Html.text "-"
+            ]
+
+            Html.button [
+                class' "button"
+                onClick (fun _ -> dispatch Increment) []
+                Html.text "+"
+            ]
+        ]
     ]
 
 app() |> Program.mountElement "hegechat-app"
